7348c7359b899e98b9526e12b1493f77c853f7df
speedy-hussu <speedyhussu.work@gmail.com>
2025-09-21 21:41:39 +0530

    initialize template for react-native

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>App</Text>
+      </View>
+    )
+  }
+}
+
+export default App
